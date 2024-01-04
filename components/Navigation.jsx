f3133953ece8721443b3fb29f61e3472f3cb5fd6
viktorip <konej86@gmail.com>
2024-01-04 18:22:45 +0200

    Added user email to navi when logged in.

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -11,7 +11,6 @@ export default function Navigation({ className }) {
     const { loc, setLoc } = useContext(LocalizationContext);
 
     const changeLocalization = (id) => {
-        console.log("changing localization to", id, loc);
         if (id === loc) return;
         setLoc(id);
     }
@@ -19,45 +18,50 @@ export default function Navigation({ className }) {
     return (
         <div className={`sticky top-0 w-full z-10 bg-[url('/background_paper_big.png')] ${className}`}>
             <div className="flex flex-col w-full h-48 bg-[url('/navi_paper_torn.png')] bg-contain bg-no-repeat bg-top">
-                <div className="flex flex-row justify-end items-center mr-2">
-                    <div className="text-center">
-                        <LoginButton className="p-1 text-indigo-900 bg-transparent text-center text-sm w-24 hover:bg-gray-200 hover:ring" />
+                <div className="flex flex-row justify-between">
+                    <div className="ml-2 text-sm text-indigo-900">
+                        {user && <span>{user.email}</span>}
                     </div>
-                    <div>
-                        <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('fi')}>
-                            <Image
-                                src="/fi.png"
-                                width={24}
-                                height={24}
-                                alt="Suomeksi"
-                            />
-                        </button>
-                    </div>
-                    <div>
-                        <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('en')}>
-                            <Image
-                                src="/en.png"
-                                width={24}
-                                height={24}
-                                alt="In English"
-                            />
-                        </button>
-                    </div>                   
-                    <div>
-                        <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('vn')}>
-                            <Image
-                                src="/vn.png"
-                                width={24}
-                                height={24}
-                                alt="Vietnamese"
-                            />
-                        </button>
+                    <div className="flex flex-row justify-end items-center mr-2">
+                        <div className="text-center">
+                            <LoginButton className="p-1 text-indigo-900 bg-transparent text-center text-sm w-24 hover:bg-gray-200 hover:ring" />
+                        </div>
+                        <div>
+                            <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('fi')}>
+                                <Image
+                                    src="/fi.png"
+                                    width={24}
+                                    height={24}
+                                    alt="Suomeksi"
+                                />
+                            </button>
+                        </div>
+                        <div>
+                            <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('en')}>
+                                <Image
+                                    src="/en.png"
+                                    width={24}
+                                    height={24}
+                                    alt="In English"
+                                />
+                            </button>
+                        </div>
+                        <div>
+                            <button className="border-4 border-transparent rounded-full hover:border-indigo-500/50" onClick={() => changeLocalization('vn')}>
+                                <Image
+                                    src="/vn.png"
+                                    width={24}
+                                    height={24}
+                                    alt="Vietnamese"
+                                />
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className={`flex flex-row justify-center items-center space-x-10 mt-4 ml-12 text-indigo-900`}>
                     <div><Link className="p-2 hover:bg-gray-200 hover:ring" href='/'>{t('navi_home_title', loc)}</Link></div>
                     <div><Link className="p-2 hover:bg-gray-200 hover:ring" href='/inbox'>{t('navi_inbox_title', loc)}</Link></div>
-                    <div><Link className="p-2 hover:bg-gray-200 hover:ring" href='/compose'>{t('navi_compose_title', loc)}</Link></div>                   
+                    <div><Link className="p-2 hover:bg-gray-200 hover:ring" href='/compose'>{t('navi_compose_title', loc)}</Link></div>
                 </div>
             </div>
 
@@ -65,16 +69,3 @@ export default function Navigation({ className }) {
         </div>
     )
 }
-
-/*
-
-<div className="absolute top-0 left-0">
-                        <Image
-                            width={500}
-                            height={120}
-                            src="/navi_paper_torn.png"
-                            alt="Navigation"
-                        />
-                    </div>
-
-*/
