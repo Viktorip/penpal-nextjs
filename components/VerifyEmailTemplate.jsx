38ffa4a9ce37bcd958f11ea6085a150ed935e022
viktorip <konej86@gmail.com>
2024-01-29 14:29:42 +0200

    Fixed background not showing in verification email and changed wording on email.

--- components/VerifyEmailTemplate.jsx
+++ components/VerifyEmailTemplate.jsx
@@ -25,9 +25,15 @@ export default function VerifyEmailTemplate({ email, fullname, loc, verifyLink }
             fontSize: "10px"
         },
         body: {
-            backgroundImage: 'url("https://swfozb1ng6po0bsb.public.blob.vercel-storage.com/email_background-KTh9ADiY7vJ0MzAcOxZHwwIbzec7rp.png")',
             color: "rgb(49 46 129)",
-            padding: "8px"
+            padding: "8px",
+            position: "relative"
+        },
+        backgroundImg: {
+            position: "absolute",
+            top: "0",
+            left: "0",
+            zIndex: "-1"
         }
     }
 
@@ -35,8 +41,7 @@ export default function VerifyEmailTemplate({ email, fullname, loc, verifyLink }
         <div>
             <img src="https://swfozb1ng6po0bsb.public.blob.vercel-storage.com/email_banner-TiVMIdxRUEDlk61DcCbkkPt5J1cIhr.png" alt="banner"></img>
             <div style={styles.body}>
-                <h1>{t('verify_email_title', loc)}</h1>
-                <br />
+                <img style={styles.backgroundImg} src="https://swfozb1ng6po0bsb.public.blob.vercel-storage.com/email_background-KTh9ADiY7vJ0MzAcOxZHwwIbzec7rp.png" alt="background"></img>
                 <p>{fullname}, {t('verify_email_body1', loc)}</p>
                 <br />
                 <p>{t('verify_email_body2', loc)} ( {email} )</p>
@@ -47,6 +52,7 @@ export default function VerifyEmailTemplate({ email, fullname, loc, verifyLink }
                 <br />
                 <br />
                 <p style={styles.linkInfo}>{t('verify_email_manual_link', loc)}</p>
+                <br />
                 <p style={styles.linkContainer}><a href={verifyLink}>{verifyLink}</a></p>
             </div>
         </div>
